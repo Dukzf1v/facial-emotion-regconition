f36c9ee78f68e56de716c1ebd2f85cef8854d576
migDuk <143349960+Dukzf1v@users.noreply.github.com>
2025-05-11 17:45:52 +0700

    update

--- src/app.py
+++ src/app.py
@@ -8,7 +8,6 @@ import cv2
 import io
 from torchvision import transforms, models
 from torchvision.models.resnet import ResNet18_Weights
-import os
 
 device_name = 'cuda' if torch.cuda.is_available() else 'cpu'
 device = torch.device(device_name)
@@ -25,17 +24,17 @@ model.fc = nn.Sequential(
     nn.Dropout(0.5),
     nn.Linear(1024, 7)
 )
-model.to(device)  
+model.to(device)
+model.load_state_dict(torch.load(r"D:\StudyPath\GR1\Facial-Emotion-Regconition\model\best_model.pth", map_location=device))
 
-model.load_state_dict(torch.load(r"/mount/src/fer/model/best_model.pth", map_location=device))
-model.eval() 
+model.eval()
 
 classes = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
 
-face_cascade = cv2.CascadeClassifier(r'/mount/src/fer/src/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(r'D:\StudyPath\GR1\Facial-Emotion-Regconition\src\haarcascade_frontalface_default.xml')
 
 transform = transforms.Compose([
-    transforms.Grayscale(num_output_channels=3), 
+    transforms.Grayscale(num_output_channels=3),  
     transforms.Resize((224, 224)), 
     transforms.ToTensor(), 
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) 
@@ -47,41 +46,41 @@ uploaded_file = st.file_uploader("Choose an Image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image_bytes = uploaded_file.read()
-    
+
     try:
-        image = Image.open(io.BytesIO(image_bytes))
+        image = Image.open(io.BytesIO(image_bytes))  
     except UnidentifiedImageError:
         st.error("The uploaded file is not a valid image. Please upload a valid image file.")
     else:
-        image_rgb = image.convert('RGB')
+        image_rgb = image.convert('RGB')  
         image_np = np.array(image_rgb) 
 
         gray = cv2.cvtColor(image_np, cv2.COLOR_RGB2GRAY)
 
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
         if len(faces) > 0:
-            all_probabilities = []
-            all_emotions = []
+            all_probabilities = [] 
+            all_emotions = []  
 
-            for (x, y, w, h) in faces:
+            for i, (x, y, w, h) in enumerate(faces):
                 cv2.rectangle(image_np, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-                face_crop = image_np[y:y + h, x:x + w] 
-                pil_face = Image.fromarray(face_crop)  
+                face_crop = image_np[y:y + h, x:x + w]
+                pil_face = Image.fromarray(face_crop) 
                 img_tensor = transform(pil_face).unsqueeze(0).to(device) 
 
                 with torch.no_grad():
-                    outputs = model(img_tensor)  
-                    probabilities = torch.nn.Softmax(dim=1)(outputs) 
+                    outputs = model(img_tensor) 
+                    probabilities = torch.nn.Softmax(dim=1)(outputs)  
                     predicted_class = torch.argmax(probabilities, 1) 
-                    predicted_emotion = classes[predicted_class.item()] 
+                    predicted_emotion = classes[predicted_class.item()]  
 
-                    probabilities = probabilities.cpu().numpy().flatten() 
-                    all_probabilities.append(probabilities)
-                    all_emotions.append(predicted_emotion)
+                    probabilities = probabilities.cpu().numpy().flatten()  
+                    all_probabilities.append(probabilities)  
+                    all_emotions.append(predicted_emotion)  
 
-                cv2.putText(image_np, f'{predicted_emotion}: {probabilities[predicted_class.item()]*100:.2f}%', 
+                cv2.putText(image_np, f'{i + 1}:{predicted_emotion}-{probabilities[predicted_class.item()] * 100:.2f}% ', 
                             (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
             image_with_boxes = Image.fromarray(image_np)
@@ -91,8 +90,8 @@ if uploaded_file is not None:
 
             for i, emotion in enumerate(all_emotions):
                 data = {
-                    'Emotion': emotion,
-                    'Probabilities': [f'{prob*100:.2f}%' for prob in all_probabilities[i]]
+                    'Face Index': f'Face {i + 1}',
+                    **{classes[j]: f'{prob*100:.2f}%' for j, prob in enumerate(all_probabilities[i])} 
                 }
                 probabilities_df.append(data)
 
